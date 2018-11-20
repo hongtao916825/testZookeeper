c6913edc9115d4997a19cfa5e4be7692249681fa
hongtao916703 <hongtao916703@163.com>
2018-11-20 23:26:30 +0800

    配置中心非阻塞实现

--- src/main/java/com/zk/test/curator/CuratorCrud.java
+++ src/main/java/com/zk/test/curator/CuratorCrud.java
@@ -1,20 +1,4 @@
-package com.zk.test.curator;/* ━━━━━━如来保佑━━━━━━
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　┻　　　┃
- * 　　┗━┓　　　┏━┛
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　　　┣┓
- * 　　　　┃　　　　　　　┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┗┻┛　┗┻┛
- * ━━━━━━永无BUG━━━━━━
- * 图灵学院-悟空老师
- * 以往视频加小乔老师QQ：895900009
- * 悟空老师QQ：245553999
- */
+package com.zk.test.curator;
 
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
--- src/test/java/com/test/Test.java
+++ src/test/java/com/test/Test.java
@@ -1,20 +1,4 @@
-package com.test;/* ━━━━━━如来保佑━━━━━━
- * 　　　┏┓　　　┏┓
- * 　　┏┛┻━━━┛┻┓
- * 　　┃　　　━　　　┃
- * 　　┃　┳┛　┗┳　┃
- * 　　┃　　　┻　　　┃
- * 　　┗━┓　　　┏━┛
- * 　　　　┃　　　┗━━━┓
- * 　　　　┃　　　　　　　┣┓
- * 　　　　┃　　　　　　　┏┛
- * 　　　　┗┓┓┏━┳┓┏┛
- * 　　　　　┗┻┛　┗┻┛
- * ━━━━━━永无BUG━━━━━━
- * 图灵学院-悟空老师
- * 以往视频加小乔老师QQ：895900009
- * 悟空老师QQ：245553999
- */
+package com.test;
 
 import com.alibaba.fastjson.JSON;
 import com.zk.test.api.Configration;
